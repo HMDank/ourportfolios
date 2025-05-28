7bde1eb29a3b93f0c0909081d0a9cb3205a763d5
Minh Dang Hoang <hminhdang05@gmail.com>
2025-05-28 20:14:10 +0200

    feat(landing): add navbar and cards

--- ourportfolios/ourportfolios.py
+++ ourportfolios/ourportfolios.py
@@ -1,39 +1,61 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
 
-from rxconfig import config
+from .components.navbar import navbar
+from .components.news_card import news_card
 
 
 class State(rx.State):
-    """The app state."""
+    cards: list[str] = ["News 1", "News 2", "News 3"]
+    show_cards: bool = False
 
-    ...
+    def load_more(self):
+        next_num = len(self.cards) + 1
+        self.cards += [f"News {next_num}",
+                       f"News {next_num+1}", f"News {next_num+2}"]
+
+    def on_scroll(self, event=None):
+        self.show_cards = True
 
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
+    return rx.fragment(
+        navbar(),
+        rx.color_mode.button(position="bottom-left"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
+            # Header and search bar centered
+            rx.vstack(
+                rx.heading("OurPortfolios", size="9"),
+                rx.text("Build your portfolios. We'll build ours"),
+                rx.input(
+                    rx.input.slot(rx.icon(tag="search")),
+                    placeholder="Try searching for a ticker here!",
+                ),
+                spacing="5",
+                justify="center",
+                align="center",
+                min_height="85vh",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
+            rx.cond(
+                State.show_cards,
+                rx.hstack(
+                    # Changed from vstack to hstack for horizontal layout
+                    rx.foreach(State.cards, news_card),
+                    spacing="5",
+                    align="center",
+                ),
             ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+            spacing="0",
+            on_scroll=State.on_scroll,
+            align="center",
+            height="200vh",
+            overflow_y="auto",
         ),
-        rx.logo(),
     )
 
 
-app = rx.App()
+app = rx.App(
+    theme=rx.theme(
+        accent_color="violet"
+    )
+)
 app.add_page(index)
