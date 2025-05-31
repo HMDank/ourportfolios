c60329441cdb9c40a4f00975e9b189fc84950841
Minh Dang Hoang <hminhdang05@gmail.com>
2025-05-31 20:16:50 +0200

    feat(select): add drawer

--- ourportfolios/pages/select.py
+++ ourportfolios/pages/select.py
@@ -1,11 +1,35 @@
 import reflex as rx
-
-
-class Domain1State(rx.State):
-    # State variables and event handlers for this domain
-    pass
+from ..components.navbar import navbar
+from ..components.drawer import drawer_button
 
 
 @rx.page(route="/select")
-def select():
-    return rx.text("Select Page")
+def index():
+    return rx.vstack(
+        navbar(),
+        rx.center(
+            rx.card(
+                rx.heading(
+                    "Select",
+                    size="7",
+                    weight="bold",
+                ),
+                rx.text(
+                    "Manually select your tickers here!",
+                    size="3",
+                    margin_bottom="0.2em"
+                ),
+                padding="2em 2.5em",
+                width="28em",
+                min_width="18em",
+                border="none",
+                background_color="transparent",
+                style={
+                    "backdropFilter": "blur(14px)",
+                },
+                spacing="4",
+            ),
+            min_height="60vh"
+        ),
+        drawer_button(),
+    )
