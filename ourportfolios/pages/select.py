68d4a86af0042bde89296285dd5b2cf78361ee38
Minh Dang Hoang <hminhdang05@gmail.com>
2025-06-01 00:03:07 +0200

    feat: add arrows

--- ourportfolios/pages/select.py
+++ ourportfolios/pages/select.py
@@ -3,18 +3,24 @@ from ..components.navbar import navbar
 from ..components.drawer import drawer_button
 from ..components.page_roller import card_roller
 
+
 @rx.page(route="/select")
 def index():
     return rx.vstack(
         navbar(),
         rx.center(
             card_roller(
-                rx.vstack(
-                    rx.heading("Recommend", weight="bold", size="7"),
-                    rx.text("caijdo", size="2"),
+                rx.hstack(
+                    rx.icon("chevron_left", size=32),
+                    rx.vstack(
+                        rx.heading("Recommend", weight="bold", size="6"),
+                        rx.text("caijdo", size="1"),
+                        align="center",
+                        justify="center",
+                        height="100%",
+                    ),
                     align="center",
                     justify="center",
-                    height="100%",
                 ),
                 rx.vstack(
                     rx.heading("Select", weight="bold", size="8"),
@@ -23,12 +29,17 @@ def index():
                     justify="center",
                     height="100%",
                 ),
-                rx.vstack(
-                    rx.heading("Simulate", weight="bold", size="7"),
-                    rx.text("caijdo", size="2"),
+                rx.hstack(
+                    rx.vstack(
+                        rx.heading("Simulate", weight="bold", size="6"),
+                        rx.text("caijdo", size="1"),
+                        align="center",
+                        justify="center",
+                        height="100%",
+                    ),
+                    rx.icon("chevron_right", size=32),
                     align="center",
                     justify="center",
-                    height="100%",
                 ),
             ),
             min_height="0vh",
