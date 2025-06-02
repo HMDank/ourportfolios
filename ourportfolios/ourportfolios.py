8edf768b270dfd9f07bd9a43861d0f1f49e26446
Minh Dang Hoang <hminhdang05@gmail.com>
2025-06-02 21:47:48 +0200

    chore: rename function

--- ourportfolios/ourportfolios.py
+++ ourportfolios/ourportfolios.py
@@ -1,6 +1,6 @@
 import reflex as rx
 from .pages import landing, select, landing_ticker  # MUST BE IMPORTED!!!
-from .utils.load_raw_data import load_data_vni
+from .utils.populate_db import load_data_vni
 
 
 app = rx.App(
