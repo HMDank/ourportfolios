c431eb9c382fafebac3282b4f495fa21865d8693
Minh Dang Hoang <hminhdang05@gmail.com>
2025-06-12 20:41:34 +0200

    chore: add analyze to app

--- ourportfolios/ourportfolios.py
+++ ourportfolios/ourportfolios.py
@@ -1,6 +1,6 @@
 import reflex as rx
 
-from .pages import landing, select, landing_ticker  # MUST BE IMPORTED!!!
+from .pages import landing, select, landing_ticker, analyze  # MUST BE IMPORTED!!!
 
 
 app = rx.App(
