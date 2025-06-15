34f5150e565792a3711a90330768920bce1fa25a
Minh Dang Hoang <hminhdang05@gmail.com>
2025-06-16 01:22:41 +0200

    chore: reformat code

--- ourportfolios/utils/compute_instrument.py
+++ ourportfolios/utils/compute_instrument.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from typing import List, Dict, Any
 
+
 def compute_ma(df: pd.DataFrame, ma_period: int) -> List[Dict[str, Any]]:
     """Calculates the Moving Average (MA)."""
     df = df.copy()
@@ -21,4 +22,3 @@ def compute_rsi(df: pd.DataFrame, rsi_period: int) -> List[Dict[str, Any]]:
     rs = np.where(df["avg_loss"] == 0, np.inf, df["avg_gain"] / df["avg_loss"])
     df["value"] = (100 - (100 / (1 + rs))).round(2)
     return df[["time", "value"]].to_dict("records")
-    
