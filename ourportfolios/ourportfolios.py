c02f40adf5dca55aab2f631c38f6c10904e36399
Minh Dang Hoang <hminhdang05@gmail.com>
2025-06-20 17:56:17 +0200

    feat(industry): add landing page placeholder

--- ourportfolios/ourportfolios.py
+++ ourportfolios/ourportfolios.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 from .utils.load_data import populate_db
 
-from .pages import landing, select, landing_ticker, analyze, compare  # MUST BE IMPORTED!!!
+from .pages import landing, select, landing_ticker, landing_industry, analyze, compare   # MUST BE IMPORTED!!!
 
 populate_db()
 
