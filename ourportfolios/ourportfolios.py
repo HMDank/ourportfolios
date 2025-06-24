f48b3547aa0f99d13c851e18a8543f89e04d46ec
Sevastopol12 <chkalov253@gmail.com>
2025-06-24 22:43:15 +0700

    Merge branch main

--- ourportfolios/ourportfolios.py
+++ ourportfolios/ourportfolios.py
@@ -1,8 +1,11 @@
 import reflex as rx
-from .pages import landing, select, landing_ticker  # MUST BE IMPORTED!!!
+
 from .utils.load_data import populate_db
 
+from .pages import landing, select, landing_ticker, landing_industry, analyze, compare   # MUST BE IMPORTED!!!
+
 populate_db()
+
 app = rx.App(
     theme=rx.theme(
         accent_color="violet"
