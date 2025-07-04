30f81d857dc6568f486da94ae53ae9d2dfad3f45
Minh Dang Hoang <hminhdang05@gmail.com>
2025-07-04 19:37:10 +0200

    feat: update tickers table structure

--- db/sql/ticker/tickers.sql
+++ db/sql/ticker/tickers.sql
@@ -4,6 +4,7 @@ CREATE TABLE tickers (
   id         SERIAL PRIMARY KEY,
   ticker     TEXT   NOT NULL,
   name       TEXT,
+  short_name TEXT,
   exchange   TEXT   NOT NULL,
   UNIQUE(ticker, exchange)
 );
