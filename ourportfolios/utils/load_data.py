63253b191933a7c927d57c6b20f13498668ad2d0
Sevastopol12 <chkalov253@gmail.com>
2025-07-11 06:53:09 +0700

    chore: add function to compute % change

--- ourportfolios/utils/load_data.py
+++ ourportfolios/utils/load_data.py
@@ -1,4 +1,4 @@
-from .preprocessing.events_texts import process_events_for_display
+from .preprocess_texts import process_events_for_display
 import pandas as pd
 import numpy as np
 import sqlite3
@@ -6,8 +6,8 @@ from datetime import date, timedelta
 from vnstock import Vnstock, Screener, Trading
 import warnings
 from typing import List
-warnings.filterwarnings("ignore")
 
+warnings.filterwarnings("ignore")
 
 
 data_vni_loaded = False
@@ -33,17 +33,67 @@ def populate_db() -> None:
             data_vni_loaded = True
             return
 
+    # Stocks
     screener = Screener(source="TCBS")
     default_params = {
         "exchangeName": "HOSE,HNX",
         "marketCap": (2000, 99999999999),
     }
-    df = screener.stock(default_params, limit=1700, lang="en")
+    stock_df = screener.stock(default_params, limit=1700, lang="en")
+
+    # Remove unstable columns
+    stock_df = stock_df.drop(
+        [x for x in stock_df.columns if x.startswith("price_vs")], axis=1
+    )
+
+    # Price board data
+    price_board_df = load_price_board(tickers=stock_df["ticker"].tolist())
+
+    # Result
+    df = pd.merge(
+        left=stock_df, right=price_board_df, left_on="ticker", right_on="symbol"
+    )
+
+    # Add additional instrument
+    df = compute_instrument(df)
 
     df.to_sql("data_vni", conn, if_exists="replace", index=False)
     conn.close()
     data_vni_loaded = True
     print("Data loaded successfully.")
+
+
+def load_price_board(tickers: List[str]) -> pd.DataFrame:
+    price_board_df = Trading(source="vci", symbol="ACB").price_board(
+        symbols_list=tickers
+    )
+    price_board_df.columns = price_board_df.columns.droplevel(0)  # Flatten columns
+    price_board_df = price_board_df.drop("exchange", axis=1)
+    price_board_df = price_board_df.loc[:, ~price_board_df.columns.duplicated()]
+
+    return price_board_df
+
+
+def compute_instrument(df: pd.DataFrame) -> pd.DataFrame:
+    if "match_price" in df.columns:
+        df = df.rename(columns={"match_price": "current_price"})
+
+        # latest close price - close price from previous day
+        df["price_change"] = df["current_price"] - df["ref_price"]
+        df["pct_price_change"] = (df["price_change"] / df["ref_price"]) * 100
+
+    # On the day when the market is closed
+    else:
+        df = df.rename(columns={"ref_price": "current_price"})
+        df["price_change"] = 0
+        df["pct_price_change"] = 0
+
+    # Normalize
+    df["current_price"] = round(df["current_price"] * 1e-3, 2)
+    df["price_change"] = round(df["price_change"] * 1e-3, 2)
+    df["pct_price_change"] = round(df["pct_price_change"], 2)
+
+    return df
 
 
 def load_historical_data(
