ee4aebc3fdc5e4ccfe6eb9d1b46f5ef54359d1ef
Minh Dang Hoang <hminhdang05@gmail.com>
2025-07-21 11:46:27 +0200

    Revert "Merge branch 'feat/landing-ticker/performance-charts' of https://github.com/HMDank/ourportfolios into fix/search-box/debug"
    This reverts commit 3c273f376de8b4048d0bbe5d148f59f8ade472c9, reversing
    changes made to edaab00a54f3694a860d82682ccd46fd8dcf07f3.

--- ourportfolios/utils/load_data.py
+++ ourportfolios/utils/load_data.py
@@ -254,6 +254,9 @@ async def load_company_data_async(ticker: str):
         asyncio.to_thread(load_company_news, ticker),
         asyncio.to_thread(load_officers_info, ticker),
         asyncio.to_thread(load_historical_data, ticker),
+        asyncio.to_thread(load_income_statement, ticker),
+        asyncio.to_thread(load_balance_sheet, ticker),
+        asyncio.to_thread(load_cash_flow, ticker),
     ]
     (
         overview,
@@ -262,6 +265,9 @@ async def load_company_data_async(ticker: str):
         news,
         officers,
         price_data,
+        income_statement,
+        balance_sheet,
+        cash_flow,
     ) = await asyncio.gather(*tasks)
     return {
         "overview": overview,
@@ -270,6 +276,9 @@ async def load_company_data_async(ticker: str):
         "news": news,
         "officers": officers,
         "price_data": price_data,
+        "income_statement": income_statement,
+        "balance_sheet": balance_sheet,
+        "cash_flow": cash_flow,
     }
 
 
