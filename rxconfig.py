f0d812d15d5040a25754b55a3d8ea0fccd9fe69f
Minh Dang Hoang <hminhdang05@gmail.com>
2025-08-11 17:18:15 +0200

    chore: update config

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="ourportfolios",
-    plugins=[rx.plugins.TailwindV3Plugin()],
+    plugins=[rx.plugins.TailwindV3Plugin(), rx.plugins.sitemap.SitemapPlugin()],
 )
