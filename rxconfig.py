650a5434e999e30b2b0d0419293d66e045bb7b61
Sevastopol12 <chkalov253@gmail.com>
2025-10-14 12:17:22 +0700

    feat: add sitemap plugins and update tailwind version

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="ourportfolios",
-    plugins=[rx.plugins.TailwindV3Plugin()],
+    plugins=[
+        rx.plugins.TailwindV4Plugin(),
+        rx.plugins.sitemap.SitemapPlugin(),
+        ],
 )
