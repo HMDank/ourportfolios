180465f017e5323c6ccb7e80706ba4888507b7d6
Hoang Minh Dang <109755535+HMDank@users.noreply.github.com>
2025-10-28 16:24:49 +0100

    perf(db): reduce api calls (#55)
    * chore(ruff): newl
    
    * perf(financial-statements): load seperately
    
    * fix: handle none types
    
    * chore: remove placeholder cards
    
    * chore: prune redundant functions
    
    * feat(comparison_df): add new database structure
    
    * chore: rename table
    
    * chore: rename table
    
    * chore: prune unused functions
    
    * chore(formatter): add ruff to auto format
    
    * chore: adjust interval to 24 hours
    
    * chore: define used columns for price board dataframe
    
    * chore: change schema
    
    * chore: rename column
    
    * chore: rename column
    
    * feat(db): add new fetch method
    
    * chore: migrate to new database structure
    
    * chore: remove ruff
    
    * chore: update schema name
    
    * chore: prune code
    
    * chore(compare): migrate to new database structure
    
    * chore: remove scheduler
    
    * chore(deps): prune

--- ourportfolios/ourportfolios.py
+++ ourportfolios/ourportfolios.py
@@ -1,17 +1,13 @@
 import reflex as rx
 from contextlib import asynccontextmanager
 from .utils.scheduler import db_scheduler
-from .utils.load_data import populate_db
 
 # MUST BE IMPORTED!!!
 from .pages import landing, select, landing_ticker, landing_industry, analyze, compare  # noqa: F401
 
 
-populate_db()
-
-
 @asynccontextmanager
-async def periodically_fetch_data(app):
+async def periodically_fetch_data():
     # Load data on initialize & keep fetching each 5 minutes
     db_scheduler.start()
 
